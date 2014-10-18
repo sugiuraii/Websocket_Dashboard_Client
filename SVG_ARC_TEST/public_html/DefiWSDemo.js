573457f66e2e86c6d9ba524db04d2cbc9af8a608
Sugiura K Nigo <sugiura.k.nigo@gmail.com>
2014-10-18 07:37:25 +0000

    20140525-01 Add benchmark at SVG_ARC_TEST
    git-svn-id: file:///c/Users/kuniaki/Repository/DefiSSMCOM_Websocket_Client/trunk@5 7cc1f472-cefd-7e4e-af8e-9376319497b1

--- SVG_ARC_TEST/public_html/DefiWSDemo.js
+++ SVG_ARC_TEST/public_html/DefiWSDemo.js
@@ -26,7 +26,7 @@ function connectSocketServer()
 
     appendMessage("* Connecting to server ..<br/>");
     // create a new websocket and connect
-    ws = new window[support]('ws://192.168.25.46:2012/');
+    ws = new window[support]('ws://192.168.25.32:2012/');
 
     // when data is comming from the server, this metod is called
     ws.onmessage = function (evt) 
@@ -38,27 +38,9 @@ function connectSocketServer()
             appendMessage("Invalid mode packet ..<br/>");
             return;
         }
-        var i;
-                            var val_list = msg_obj.val;
+        
+        meter_valuechange(msg_obj.val.Boost);
 
-        for ( i = 0; i < val_list.length; i++)
-        {
-            var val_obj = val_list[i];
-                var val_parameter = val_obj[0];
-                var val_value = val_obj[1];
-
-                switch (val_parameter)
-                {
-                    case 'Boost' : $('#Boost_textbox').val(val_value); meter_valuechange(val_value); break;
-                    case 'Tacho' : $('#Tacho_textbox').val(val_value); break;
-                    case 'Oil_Pres' : $('#Oil_Pres_textbox').val(val_value); break;
-                    case 'Fuel_Pres' : $('#Fuel_Pres_textbox').val(val_value); break;
-                    case 'Ext_Temp' : $('#Ext_Temp_textbox').val(val_value); break;
-                    case 'Oil_Temp' : $('#Oil_Temp_textbox').val(val_value); break;
-                    case 'Water_Temp' : $('#Water_Temp_textbox').val(val_value); break;
-                    default : appendMessage("* Packet Parse error ..<br/>");
-                }
-           }
     };
 
     // when the connection is established, this method is called
@@ -68,6 +50,10 @@ function connectSocketServer()
         $('#sendButton').attr("disabled", "");
         $('#connectButton').attr("disabled", "disabled");
         $('#disconnectButton').attr("disabled", "");
+        
+        //Enable boost
+        var boost_enable_msg = "{\"mode\":\"DEFI_WS_SEND\",\"code\":\"Boost\",\"flag\":\"true\"}";
+        ws.send(boost_enable_msg);
     };
 
     // when the connection is closed, this method is called
@@ -107,30 +93,10 @@ window.onload = function ()
     $('#messageInput').attr("disabled", "disabled");
     $('#sendButton').attr("disabled", "disabled");
     $('#disconnectButton').attr("disabled", "disabled");
-};
-
-onload = function()
-{
-    svg_obj = new CircularCustomProgressBar_svg("svg_test1","layer6");
-    svg_obj.circle_center_x = 122+178.9375;
-    svg_obj.circle_center_y = 115+157;
-    svg_obj.circle_radius_x = 80;
-    svg_obj.circle_radius_y = 80;
-    svg_obj.offset_angle = 180;
-    svg_obj.full_angle = 180;
-    svg_obj.anticlockwise = false;
-    svg_obj.max = 2;
-    svg_obj.min = -1;
-
+    initialize_gauge();
 };
 
 function meter_valuechange(val)
 {
-    svg_obj.value = val;
-    svg_obj.update();
-    /*
-    svg_obj2.value =val;
-    svg_obj2.update();
-    */
-
+    $("#gauge_frame1")[0].contentWindow.set_value(val);
 };
