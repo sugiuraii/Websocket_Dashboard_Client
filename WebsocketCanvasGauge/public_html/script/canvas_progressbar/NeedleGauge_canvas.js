86790b5b80e29eb360608efa68e1487bf81d0b76
Sugiura K Nigo <sugiura.k.nigo@gmail.com>
2014-10-18 08:28:33 +0000

    20140823 Complete AnalogMeterCluster
    git-svn-id: file:///c/Users/kuniaki/Repository/DefiSSMCOM_Websocket_Client/trunk@14 7cc1f472-cefd-7e4e-af8e-9376319497b1

--- WebsocketCanvasGauge/public_html/script/canvas_progressbar/NeedleGauge_canvas.js
+++ WebsocketCanvasGauge/public_html/script/canvas_progressbar/NeedleGauge_canvas.js
@@ -4,25 +4,21 @@
  * and open the template in the editor.
  */
 
-var NeedleGauge_canvas = function(canvas_id, image_name, img_shaft_x, img_shaft_y)
+var NeedleGauge_canvas = function(canvas, img)
 {
-    // Local variables and instances
-    var canvas = document.getElementById(canvas_id);
+    // Local value and instances
     var context = canvas.getContext('2d');
-    var img = new Image();
-    img.src = image_name;
-    var shaft_x = img_shaft_x;
-    var shaft_y = img_shaft_y;
     var curr_value;
     
     // Properties and Default values
-    this.anticlockwise = false;
-    this.max = 100;
-    this.min = 0;
     this.offset_angle = 0;
     this.full_angle = 360;
+    this.anticlockwise = false;
+    this.min = 0;
+    this.max = 100;
+    
     this.rotation_center_x = canvas.width/2;
-    this.rotation_center_y = canvas.height/2;
+    this.rotation_center_y = canvas.height/2; 
     
     this.value = this.min;
     
@@ -38,27 +34,35 @@ var NeedleGauge_canvas = function(canvas_id, image_name, img_shaft_x, img_shaft_
                 
         var canvas_max_x = canvas.width;
         var canvas_max_y = canvas.height;
-        var percent = (curr_value - this.min)*(this.max - this.min)/100;
-                
+        var rotation_center_x = this.rotation_center_x;
+        var rotation_center_y = this.rotation_center_y;
+
+        var anticlockwise = this.anticlockwise;
+        
+        var percent = (curr_value - this.min)/(this.max - this.min)*100;
+        if(percent>100)
+            percent=100;
+        else if (percent<0)
+            percent=0;
+        
         var rotation_angle;
-        if(this.anticlockwise)
+        if(anticlockwise)
         {
-            rotation_angle = Math.PI/180*(this.offset_angle - this.full_angle/100*percent);
+            rotation_angle = Math.PI/180*(this.offset_angle - this.full_angle*(percent/100));
         }
         else
         {
-            rotation_angle = Math.PI/180*(this.offset_angle + this.full_angle/100*percent);
+            rotation_angle = Math.PI/180*(this.offset_angle + this.full_angle*(percent/100));
         }
         context.save();
         // reset and clear canvas
         context.clearRect(0,0,canvas_max_x,canvas_max_y);
         
         //実際の画像変換とは逆順に定義していることに注意。
-        context.translate(this.rotation_center_x, this.rotation_center_y);
-        context.translate(shaft_x, shaft_y);     
+        context.translate(rotation_center_x, rotation_center_y);
         //apply rotation
         context.rotate(rotation_angle);        
-        context.translate( -1 * shaft_x, -1 * shaft_y);
+        context.translate( -1 * rotation_center_x, -1 * rotation_center_y);
         
         context.drawImage(img, 0,0);
         context.restore();
