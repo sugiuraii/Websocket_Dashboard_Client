4d7891eec2a04775a04be0aacfb4446e356b5aa3
Sugiura K Nigo <sugiura.k.nigo@gmail.com>
2017-02-01 13:12:56 +0000

    Merge DefiCOM/SSMCOM/FUELTRIPWebsocket.js into one file.
    git-svn-id: file:///c/Users/kuniaki/Repository/DefiSSMCOM_Websocket_Client/trunk@70 7cc1f472-cefd-7e4e-af8e-9376319497b1

---
+++ WebsocketCanvasGauge/public_html/script/websocket/DefiSSMCOMWebsocketClient.js
@@ -0,0 +1,371 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Superclass of Websocket classes.
+ * @constructor
+ * @returns {WebsocketCommon}
+ */
+var WebsocketCommon = function()
+{
+    'use strict';
+    /**
+     * Websocket obj.
+     * @private
+    */
+    this._ws;
+    /**
+     * URL of server
+     */
+    this.URL;
+    /**
+     * Event on ERR packet is received.
+     * @param {msg} msg JSON of message.
+     */
+    this.onERRpacketReceived = function(msg){};
+    /**
+     * Event on RES packet is received.
+     * @param {msg} msg JSON of message.
+     */
+    this.onRESpacketReceived = function(msg){};
+    /**
+     * Event on websocket open.
+     */
+    this.onWebsocketOpen = function(){};
+    /**
+     * Event on websocket close.
+     */
+    this.onWebsocketClose = function(){};
+    this.onWebsocketError = function(msg)
+    {
+      alert(msg);  
+    };
+};
+
+/**
+ * Connect websocket.
+ */
+WebsocketCommon.prototype.Connect = function() { 
+    'use strict';    
+    var support = "MozWebSocket" in window ? 'MozWebSocket' : ("WebSocket" in window ? 'WebSocket' : null);
+
+    if (support === null) {
+       this.onWebsocketError("Websocket is not supported.");
+        return;
+    };
+    this._ws = new window[support](this.URL); 
+    
+    // store self reference in order to register event handler.
+    var self = this;
+    // when data is comming from the server, this metod is called
+    this._ws.onmessage = function (evt) {
+        self._parseIncomingMessage(evt.data);
+    };
+    // when the connection is established, this method is called
+    this._ws.onopen = function () {
+        self.onWebsocketOpen();
+    };
+    // when the connection is closed, this method is called
+    this._ws.onclose = function () {
+        self.onWebsocketClose();
+    };
+};
+
+/**
+ * Send reset packet.
+ * @returns {undefined}
+ */
+WebsocketCommon.prototype.SendReset = function()
+{
+    'use strict';
+    var obj={
+      mode : "RESET"
+    };
+    
+    var jsonstr = JSON.stringify(obj);
+    
+    this._ws.send(jsonstr);    
+};
+
+/**
+ * Close websocket.
+ */
+WebsocketCommon.prototype.Close = function()
+{
+    'use strict';
+    if(this._ws){
+        this._ws.close();
+    };
+};
+
+/**
+ * Get websocket ready state.
+ * @return {Number} Websocket state.
+ */
+WebsocketCommon.prototype.getReadyState = function()
+{
+    'use strict';
+    if(typeof this._ws === "undefined")
+        return -1;
+    
+    return this._ws.readyState;
+};
+
+/**
+ * Superclass of Defi/SSM/Arduino/ELM327 websocket.
+ * @returns {DefiSSMWebsocketCommon}
+ */
+var DefiSSMWebsocketCommon = function()
+{
+    'use strict';
+    WebsocketCommon.call(this);    
+    /**
+     * Prefix of modecode. (Do not change).
+     */
+    this.ModePrefix;
+    
+    /**
+     * Switch to take packet interval time.
+     */
+    this.RecordIntervalTimeIsEnabled = true;
+    /**
+     * Timestamp of previous arrival of VAL packet.
+     * @private 
+     */
+    this._valPacketPreviousTimeStamp = window.performance.now();
+    /**
+     * VAL packet interval time.
+     * @private
+     */
+    this._valPacketIntervalTime = 0;
+    
+    var self = this;
+    Object.defineProperty(this, "ValPacketIntervalTime", 
+    {
+        get : function()
+        {
+            return self._valPacketIntervalTime;
+        }
+    });
+    /**
+     * Event on VAL packet is received.
+     * @param {VALPacketJSON} val Incoming val packet data.
+     */
+    this.onVALpacketReceived = function(val){};
+};
+Object.setPrototypeOf(DefiSSMWebsocketCommon.prototype, WebsocketCommon.prototype);
+
+/**
+ * Parse incomming JSON mesage
+ * @param {JSON} message JSON message to parse.
+ */
+DefiSSMWebsocketCommon.prototype._parseIncomingMessage = function(message){
+    'use strict';
+    var received_json_object = JSON.parse(message);    
+    
+    switch(received_json_object.mode)
+    {
+        case ("VAL") :
+            if(this.RecordIntervalTimeIsEnabled)
+            {
+                var nowTime = window.performance.now();
+                this._valPacketIntervalTime = nowTime - Number(this._valPacketPreviousTimeStamp);
+                this._valPacketPreviousTimeStamp = nowTime;
+            };
+            
+            for (var key in received_json_object.val)
+            {
+                if(key in this.onVALpacketReceived)
+                    this.onVALpacketReceived[key](received_json_object.val[key]);
+            }
+            break;
+        case("ERR"):
+            this.onERRpacketReceived(received_json_object.msg);
+            break;
+        case("RES"):
+            this.onRESpacketReceived(received_json_object.msg);
+            break;
+        default:
+            this.onWebsocketError("Unknown mode packet received. " + message);
+    };
+};
+
+/**
+ * DefiCOM_Websocket class.
+ * @constructor
+ * @returns {DefiCOM_Websocket}
+ */
+var DefiCOM_Websocket = function()
+{
+    'use strict';
+    DefiSSMWebsocketCommon.call(this);
+    /**
+     * Prefix of modecode. (Do not change).
+     */
+    this.ModePrefix = "DEFI";
+};
+Object.setPrototypeOf(DefiCOM_Websocket.prototype, DefiSSMWebsocketCommon.prototype);
+
+/**
+ * Send DEFI(ARDUINO)_WS_SEND to enable or disable defi(arduino) parameter.
+ * @param {String} code Defi(Arduino) parameter code to set.
+ * @param {String} flag Enable or disable flag. ("true" or "false")
+ */
+DefiCOM_Websocket.prototype.SendWSSend = function(code,flag)
+{
+    'use strict';
+    var obj={
+      mode : this.ModePrefix + "_WS_SEND",
+      code : code,
+      flag : flag
+    };
+    
+    var jsonstr = JSON.stringify(obj);
+    this._ws.send(jsonstr);
+};
+
+/**
+ * Send DEFI(ARDUINO)_WS_INTERVAL to set inverval for sending VAL message.
+ * @param {Numeric} interval of VAL message. (more than 0. 0:No interval).
+ */
+DefiCOM_Websocket.prototype.SendWSInterval = function(interval)
+{
+    'use strict';
+    var obj={
+      mode : this.ModePrefix + "_WS_INTERVAL",
+      interval : interval
+    };
+    
+    var jsonstr = JSON.stringify(obj);
+    
+    this._ws.send(jsonstr);    
+};
+
+/**
+ * ArduinoCOM_Websocket class.
+ * @constructor
+ */
+var ArduinoCOM_Websocket = function()
+{
+    'use strict';
+    DefiCOM_Websocket.call(this);
+    this.ModePrefix = "ARDUINO"; 
+};
+Object.setPrototypeOf(ArduinoCOM_Websocket.prototype, DefiCOM_Websocket.prototype);
+
+var SSMCOM_Websocket = function()
+{
+    'use strict';
+    DefiSSMWebsocketCommon.call(this);
+    this.ModePrefix = "SSM";
+};
+Object.setPrototypeOf(SSMCOM_Websocket.prototype, DefiSSMWebsocketCommon.prototype);
+
+SSMCOM_Websocket.prototype.SendCOMRead = function(code, mode, flag)
+{
+    'use strict';
+    var obj={
+      mode : this.ModePrefix + "_COM_READ",
+      code : code,
+      read_mode : mode,
+      flag : flag
+    };
+    
+    var jsonstr = JSON.stringify(obj);
+    this._ws.send(jsonstr);
+};
+
+SSMCOM_Websocket.prototype.SendSlowreadInterval = function(interval)
+{
+    'use strict';
+    var obj={
+      mode : this.ModePrefix + "_SLOWREAD_INTERVAL",
+      interval : interval
+    };
+    
+    var jsonstr = JSON.stringify(obj);
+    
+    this._ws.send(jsonstr);    
+};
+
+var ELM327COM_Websocket = function()
+{
+    'use strict';
+    SSMCOM_Websocket.call(this);
+    this.ModePrefix = "ELM327";
+};
+Object.setPrototypeOf(ELM327COM_Websocket.prototype, SSMCOM_Websocket.prototype);
+
+var FUELTRIP_Websocket = function()
+{
+    'use strict';
+    this.ModePrefix = "FUELTRIP";
+    WebsocketCommon.call(this);
+    this.onMomentFUELTRIPpacketReceived = function(moment_gasmilage, total_gas, total_trip, total_gasmilage){};
+    this.onSectFUELTRIPpacketReceived = function(sect_span, sect_trip, sect_gas, sect_gasmilage){};
+};
+Object.setPrototypeOf(FUELTRIP_Websocket.prototype, WebsocketCommon.prototype);
+
+FUELTRIP_Websocket.prototype.SendSectSpan = function(sect_span)
+{
+    var obj={
+      mode : "SECT_SPAN",
+      sect_span : sect_span
+    };
+    
+    var jsonstr = JSON.stringify(obj);
+    this._ws.send(jsonstr);
+};
+
+FUELTRIP_Websocket.prototype.SendReset = function()
+{
+    var obj={
+      mode : "RESET"
+    };
+    
+    var jsonstr = JSON.stringify(obj);
+    
+    this._ws.send(jsonstr);    
+};
+
+FUELTRIP_Websocket.prototype.SendSectStoreMax = function(storemax)
+{
+    var obj={
+      mode : "SECT_STOREMAX",
+      storemax : storemax
+    };
+    
+    var jsonstr = JSON.stringify(obj);
+    
+    this._ws.send(jsonstr);    
+};
+
+FUELTRIP_Websocket.prototype._parseIncomingMessage = function(message){
+    var received_json_object = JSON.parse(message);
+    switch(received_json_object.mode)
+    {
+        case ("MOMENT_FUELTRIP") :
+            this.onMomentFUELTRIPpacketReceived(received_json_object.moment_gasmilage,
+            received_json_object.total_gas,
+            received_json_object.total_trip,
+            received_json_object.total_gasmilage);
+            break;
+        case ("SECT_FUELTRIP") :
+            this.onSectFUELTRIPpacketReceived(received_json_object.sect_span,
+            received_json_object.sect_trip,
+            received_json_object.sect_gas,
+            received_json_object.sect_gasmilage);
+            break;
+        case("ERR"):
+            this.onERRpacketReceived(received_json_object.msg);
+            break;
+        case("RES"):
+            this.onRESpacketReceived(received_json_object.msg);
+            break;
+        default:
+            this.onWebsocketError("Unknown mode packet received. " + message);
+    };
+};
